7eac872951b8130d83fb32fa2cc5faf00fe198cd
Elizabeth <sinenkova.04@mail.ru>
2022-12-14 16:43:04 +0000

    дз Елизавета Синенкова
    мое дз

--- homework_01/task_01/src/utils.cpp
+++ homework_01/task_01/src/utils.cpp
@@ -1,7 +1,40 @@
-#include "utils.hpp"
-
+#include "utils.hpp" 
+#include <iostream>
 #include <stack>
-
+ 
 std::vector<std::string> SplitString(const std::string& data) {
-  return {data};
-}
+  std::vector<std::string> elements;
+  std::string element = "";
+  int flag = 0;
+  for (char const& literal: data) {
+    if (flag == 0) {
+      if (literal == '(') {
+        flag = 1;
+      } else if (literal != ' ' and literal != '\t') {
+        element.push_back(literal);
+      } else if (literal == ' ' or literal == '\t') {
+        if (!element.empty()) {
+          elements.push_back(element);
+          element.clear();
+        }
+      };
+    };
+    if (flag == 1) {
+      if (literal != ')') {
+        element.push_back(literal);
+      } else if (literal == ')') {
+        element.push_back(literal);
+        if (!element.empty()) {
+          elements.push_back(element);
+          element.clear();
+        }
+        flag = 0;
+      };
+    };
+  };
+  if (!element.empty()) {
+    elements.push_back(element);
+    element.clear();
+  }
+  return {elements};
+};
